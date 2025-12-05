8ed10712f8ed11943c792a9c82d3b11e4808462a
PolisTP98 <123047038@upq.edu.mx>
2025-12-05 16:30:09 -0600

    Base de Datos

--- Manheru/Pagina.sql
+++ Manheru/Pagina.sql
@@ -17,35 +17,35 @@ Use Pagina;
 Go
 
 --- Plantilla Tipos
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablaTipos.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablaTipos.sql"
 
 --- Plantilla Productos
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablaProductos.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablaProductos.sql"
 
 --- Plantilla Roles
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablaRoles.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablaRoles.sql"
 
 -- Plantilla Pagina
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablasPagina.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablasPagina.sql"
 
 --- Plantilla Usuarios
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablaUsuarios.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablaUsuarios.sql"
 
 -- Plantilla Cantidades
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablaCantidades.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablaCantidades.sql"
 
 --- Plantilla Ventas
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\TablaVentas.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\TablaVentas.sql"
 
 --- Errores
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Errores\EstructuraDeErrores.sql"
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Errores\DatosDeErrores.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Errores\EstructuraDeErrores.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Errores\DatosDeErrores.sql"
 
 -- Mensajes de error
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Funcionamiento\Procedimientos almacenados\MensajesDeError.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Funcionamiento\Procedimientos almacenados\MensajesDeError.sql"
 
 --- Insertar SP
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Funcionamiento\Procedimientos almacenados\InsertarRegistro.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Funcionamiento\Procedimientos almacenados\InsertarRegistro.sql"
 
 --- Datos Base
-:r "C:\Users\ianrd\OneDrive\Documentos\BD\Manheru\Plantillas\DatosBase.sql"
+:r "D:\GitHub\ManHeru_2.0\Manheru\Plantillas\DatosBase.sql"
